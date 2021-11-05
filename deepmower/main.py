33126a1728d411652a55c76790b9abc5b741bc88
Jimmy Risk <jrisk@cpp.edu>
2021-11-05 01:21:41 -0700

    hacky reward function implemented as temporary fix

--- deepmower/main.py
+++ deepmower/main.py
@@ -16,8 +16,8 @@ retro.data.Integrations.add_custom_path(LAWNMOWER_LOCATION)
 
 try:
     env = retro.make(game='lawnmower',
-                    state='lawnmower.lawn1.state', \
-                    inttype=retro.data.Integrations.ALL)
+                     state='lawnmower.lawn1.state', \
+                     inttype=retro.data.Integrations.ALL)
 except FileNotFoundError:
     print(f"ERROR: lawnmower integration directory not found in the following location: {LAWNMOWER_LOCATION}")
     sys.exit()
@@ -25,18 +25,18 @@ except FileNotFoundError:
 ### OBSERVATION WRAPPERS
 
 action_space = [
-        ['LEFT' ,'B'], 
-        ['RIGHT','B'], 
-        ['DOWN' ,'B'], 
-        ['UP'   ,'B']
-    ]
+    ['LEFT', 'B'],
+    ['RIGHT', 'B'],
+    ['DOWN', 'B'],
+    ['UP', 'B']
+]
 
 env = Discretizer(env, combos=action_space)
 env = ResizeObservation(env, shape=84)
 env = GrayScaleObservation(env, keep_dim=False)
 env = TransformObservation(env, f=lambda x: x / 255.)
 env = FrameStack(env, num_stack=4)
-env = SkipFrame(env, skip=4)
+env = SkipFrame(env, skip=2)
 
 ### CHECK NVIDIA CUDA AVAILABILITY
 
@@ -47,7 +47,7 @@ print(f"Using CUDA: {use_cuda}\n")
 
 save_dir = Path("../checkpoints") / datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
 save_dir.mkdir(parents=True)
-#checkpoint = Path('checkpoints\\2021-11-04T00-10-01\\Hank_net_0.chkpt')
+#checkpoint = Path('..\\checkpoints\\2021-11-05T00-18-09\Hank_net_0.chkpt')
 hank = Hank(state_dim=(4, 84, 84), action_dim=env.action_space.n, save_dir=save_dir)#, checkpoint=checkpoint)
 
 ### LOGGING
@@ -56,18 +56,27 @@ logger = MetricLogger(save_dir)
 
 ### BEGIN TRAINING
 
-episodes = 1000
+debug = True
+
+episodes = 100000
+
+best_reward = 0
+
 for e in range(episodes):
     # State reset between runs
     state = env.reset()
-    
+
     # Variables to keep track of for reward function
-    frame_count = 0
+    frame_count: int = 0
     fuel_pickups = 0
     turns = 0
-    
+
+    new_best = False
+
     prev_info = None
     info = None
+
+    rewrd = 0
 
     # Episode training
     while True:
@@ -81,35 +90,49 @@ for e in range(episodes):
         # Agent performs action
         next_state, reward, done, info = env.step(action)
 
-        # Reward function information
+        # reward function information
         if prev_info is not None:
             if info["GAME_FUEL"] > prev_info["GAME_FUEL"] and frame_count > 65:
                 fuel_pickups += 1
                 cur_fuel_pickup = 1
-                fuel_rew = cur_fuel_pickup*100*(1-1/(1+np.exp(-frame_count/600)))
-                print(f"Frame: {frame_count}, Reward: {fuel_rew}")
+                fuel_rew = cur_fuel_pickup * 100 * (1 - 1 / (1 + np.exp(-frame_count / 600)))
+                print(f"Frame: {frame_count}, reward: {fuel_rew}")
             if info["DIR"] != prev_info["DIR"]:
                 turns += 1
             else:
                 turns = 0
+            if info["DONE"] > prev_info["DONE"]:
+                rewrd += 1
 
         # Penalizes for turning too much
-        reward -= (turns-1)*turns/1000
+        rewrd -= (turns - 1) * turns / 1000
 
-        # Reward for fuel pickup
-        reward += fuel_rew
+        # reward for fuel pickup
+        rewrd += fuel_rew
 
         # Penalizes for taking too long
-        reward -= frame_count/100000
+        rewrd -= frame_count / 100000
 
         # Remember
-        hank.cache(state, next_state, action, reward, done)
+        hank.cache(state, next_state, action, rewrd, done)
+
+        if debug is True:
+            if e > 0:
+                pass
+                #print(f"Reward = {reward}")
+                #print(f"Turns = {turns}")
+                #print("~~~current")
+                #print(info)
+                #print("~~~previous")
+                #print(prev_info)
+                #print("~~~")
+                #print("~~~")
 
         # Learn
         q, loss = hank.learn()
 
         # Logging
-        logger.log_step(reward, loss, q)
+        logger.log_step(rewrd, loss, q)
 
         # Update state
         state = next_state
@@ -122,7 +145,13 @@ for e in range(episodes):
 
         # Check if end condition is reached
         if done or info["GAME_FUEL"] == 0:
+            if rewrd >= best_reward:
+                best_reward = rewrd
+                new_best = True
+                print(f"~~~ NEW BEST!  Good job, Hank!  Top Propane Points = {best_reward}")
             break
+
+
 
     logger.log_episode()
 
